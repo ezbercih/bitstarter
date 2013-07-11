a237a2de1b945dc14eb419ada5ccaf3aab179b3d
Cem Ezberci <cem.ezberci@gmail.com>
2013-07-11 01:43:34 +0000

    modified web.js to read and return content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var content = fs.readFileSync('index.html').toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
